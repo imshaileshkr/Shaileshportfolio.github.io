060a0d19e592ee373bc03e4212521a7332c57af2
imshaileshkr <imshaileshkr@users.noreply.github.com>
2020-10-28 16:01:11 +0530

    Update

--- js/script.js
+++ js/script.js
@@ -85,3 +85,64 @@ const filterContainer = document.querySelector(".portfolio-filter"),
             }
            
         })
+
+
+        // ********  Aside Navbar ********
+
+            const nav=document.querySelector(".nav"),
+                  navList=nav.querySelectorAll("li"),
+                  totalNavList=navList.length,
+                  allSection=document.querySelectorAll(".section"),
+                  totalSection = allSection.length;
+                
+            for(let i=0; i<totalNavList; i++){
+                const a=navList[i].querySelector("a");
+                // console.log(a);
+                a.addEventListener("click", function() {
+                    // console.log(this);
+                    
+                    // remove back section class
+                    for(let i=0;i<totalSection;i++){
+                        allSection[i].classList.remove("back-section");
+                    }
+                    for(let j=0; j<totalNavList; j++)
+                    {    
+                        if(navList[j].querySelector("a").classList.contains("active")){
+                        //    Add back section Class
+                            allSection[j].classList.add("back-section");
+                        }
+                        navList[j].querySelector("a").classList.remove("active");
+                    }
+                    this.classList.add("active");
+                    showSection(this);
+
+                    if(window.innerWidth < 1200){
+                        asideSectionTogglerBtn();
+                    }
+                })
+            }
+
+            function showSection(element){
+                // remove class active from all section
+                for(let i=0;i<totalSection;i++){
+                    allSection[i].classList.remove("active");
+                }
+                const target=element.getAttribute("href").split("#")[1];
+              document.querySelector("#"+target).classList.add("active")  
+            }
+
+    const navTogglerBtn = document.querySelector(".nav-toggler"),
+          aside= document.querySelector(".aside");
+    
+    navTogglerBtn.addEventListener("click", () =>{
+        asideSectionTogglerBtn();
+
+    })
+
+    function asideSectionTogglerBtn() {
+        aside.classList.toggle("open");
+        navTogglerBtn.classList.toggle("open");
+        for(let i=0;i<totalSection;i++){
+            allSection[i].classList.toggle("open");
+        }
+    }
